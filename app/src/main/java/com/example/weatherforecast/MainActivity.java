ee30d7057f276da4a2796e0ade4abdcddbc17937
GaliakhmetovNI <nnngaliahmetov@gmail.com>
2022-04-17 22:33:12 +0300

    add search panel

--- app/src/main/java/com/example/weatherforecast/MainActivity.java
+++ app/src/main/java/com/example/weatherforecast/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.weatherforecast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText searchField;
+    private ImageButton searchImageButton;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchField = findViewById(R.id.et_search_city);
+        searchImageButton = findViewById(R.id.ib_search);
+        result = findViewById(R.id.tv_result);
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                result.setText("Работает!");
+            }
+        };
+
+        searchImageButton.setOnClickListener(onClickListener);
+
     }
 }
